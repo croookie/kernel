29d5fa69fbae82ab79814e3c6f29af84711a4e03
crookie <mkrulikovskyi@gmail.com>
2026-09-23 15:18:10 +0200

    feat: add helper print_vga function and output loaded kernel size info

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
 [bits 32]
 
+section .data
+size_output db "Loaded kernel size: ", 0
+
 section .multiboot
 align 4
 	dd 0x1BADB002
@@ -22,11 +25,52 @@ _start:
 	mov byte [0xb800c], 'd'
 	mov byte [0xb800d], 0x0f
 
+	mov esp, stack_top
+
 	extern kernel_start
 	extern kernel_end
 
-	mov eax, kernel_start
-	mov ebx, kernel_end
+	mov eax, kernel_end
+	sub eax, kernel_start
+	mov ebx, kernel_size
+
+	; takes input number from eax,
+	; puts the output string at address pointed to by ebx
+	itoa:
+		; push ebp and store esp there for bounds checking in .pop
+		push ebp
+		mov ebp, esp
+	.loop:
+		xor edx, edx
+		mov ecx, 10
+		div ecx
+		push edx
+
+		; end loop if dividend = 0
+		test eax, eax
+		jne .loop
+
+	.pop:
+		pop eax
+		add eax, '0'
+		mov [ebx], al
+		inc ebx
+		cmp ebp, esp
+		jne .pop
+
+	.end:
+		pop ebp
+
+	push size_output
+	push 0xb80a2
+	call print_vga
+	add esp, 8
+
+	push kernel_size
+	push 0xb80ca
+	call print_vga
+	add esp, 8
+
 	; entering long mode
 	;mov eax, cr0
 	;and eax, 0x7fffffff
@@ -38,13 +82,38 @@ _start:
 	hlt
 	jmp $
 
+	; prints characters to screen using vga text mode with white on black color
+	; first arg - address of string to print;
+	; second arg - address of vga at which to start printing
+	print_vga:
+		push ebp
+		mov ebp, esp
+		mov ebx, [ebp+12]
+		mov eax, [ebp+8]
+	.loop:
+		mov cl, [ebx]
+		mov [eax], cl
+		inc eax
+		mov byte [eax], 0x0f
+		inc eax
+
+		inc ebx
+		cmp byte [ebx], 0
+		jne .loop
+
+		; epilogue
+		pop ebp
+		ret
+
 section .bss
-align 16
+kernel_size:
+	resb 5
+alignb 16
 stack_bottom:
 	resb 16384
 stack_top:
 
-align 4096
+alignb 4096
 pml4:
 	resb 4096
 pdpt:
